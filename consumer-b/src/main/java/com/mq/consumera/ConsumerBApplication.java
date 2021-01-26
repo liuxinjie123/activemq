819a387caf490768c51079b6516611072705b678
liuxinjie123 <jackdream@163.com>
2021-01-26 22:17:18 +0800

    activemq

---
+++ consumer-b/src/main/java/com/mq/consumera/ConsumerBApplication.java
@@ -0,0 +1,12 @@
+package com.mq.consumera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerBApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerBApplication.class, args);
+    }
+
+}
